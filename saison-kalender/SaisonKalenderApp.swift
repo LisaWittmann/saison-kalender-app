72a5caa8258997606a94831290cc7460b3c34664
Lisa Wittmann <lisa.wittmann@student.hs-rm.de>
2022-03-26 11:37:59 +0100

    chore: add application assets

---
+++ saison-kalender/SaisonKalenderApp.swift
@@ -0,0 +1,20 @@
+//
+//  saison_kalenderApp.swift
+//  saison-kalender
+//
+//  Created by Lisa Wittmann on 26.03.22.
+//
+
+import SwiftUI
+
+@main
+struct SaisonKalenderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
